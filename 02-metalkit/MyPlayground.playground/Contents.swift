350d74b41977aa1ff39f7e0574dc11261e5ed716
aaron <aaron.a@me.com>
2025-01-15 23:28:50 -0600

    shaders, commandqueue
    Signed-off-by: aaron <aaron.a@me.com>

--- 02-metalkit/MyPlayground.playground/Contents.swift
+++ 02-metalkit/MyPlayground.playground/Contents.swift
@@ -5,6 +5,7 @@ guard let device = MTLCreateSystemDefaultDevice() else { fatalError("gpu not sup
 
 let frame = CGRect(x: 0, y: 0, width: 600, height: 600)
 let view = MTKView(frame: frame, device: device)
+
 //setting the background color as cream
 view.clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 0.8, alpha: 1.0)
 
@@ -16,6 +17,35 @@ let mesh = try MTKMesh(mesh: mdlMesh, device: device)
 
 //queues, buffers, and encoders
 
+guard let commandQueue = device.makeCommandQueue() else { fatalError("command queue could not be created.") }
+
+//creating vertex and frag shaders. this would typically go in a different file with a .metal extension.
+let shader = """
+#include <metal_stdlib>
+using namespace metal;
+
+struct VertexIn {
+    float4 position [[attribute(0)]];
+};
+
+vertex float4 vertex_main(const VertexIn vertex_in [[stage_in]])
+{
+    return vertex_in.position;
+}
+
+fragment float4 fragment_main() {
+    return float4(1, 0, 0, 1);
+}
+
+"""
+
+//making a library of our shader, and then making functions of the vert and frag shaders.
+let library = try device.makeLibrary(source: shader, options: nil)
+let vertexFunction = library.makeFunction(name: "vertex_main")
+let fragmentFunction = library.makeFunction(name: "fragment_main")
+
 
 
 print("yay")
+
+

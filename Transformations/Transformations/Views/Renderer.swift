350821c9e5e0a95636ebe12a2ea01f0b2cb64f2a
aaron <aaron.a@me.com>
2025-02-05 23:40:57 -0600

    renderer setup
    Signed-off-by: aaron <aaron.a@me.com>

--- Transformations/Transformations/Views/Renderer.swift
+++ Transformations/Transformations/Views/Renderer.swift
@@ -36,8 +36,40 @@ class Renderer: NSObject, MTKViewDelegate {
         
         self.mesh = TriangleMesh(metalDevice: metalDevice)
         
+        self.scene = RenderScene()
         
+        super.init()
+    }
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         
+    }
+    
+    func draw(in view: MTKView) {
+        scene.update()
         
+        guard let drawable = view.currentDrawable else {
+            fatalError("View not drawabale")
+        }
+        
+        let commandBuffer = metalCommandQueue.makeCommandBuffer()
+        
+        let renderPassDescriptor = view.currentRenderPassDescriptor
+        renderPassDescriptor?.colorAttachments[0].clearColor = MTLClearColorMake(0, 0.5, 0.5, 1.0)
+        renderPassDescriptor?.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor?.colorAttachments[0].storeAction = .store
+        
+        let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor!)
+        
+        for _ in scene.triangles {
+            renderEncoder?.setRenderPipelineState(pipelineState)
+            renderEncoder?.setVertexBuffer(mesh.vertexBuffer, offset: 0, index: 0)
+            renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
+        }
+        
+        renderEncoder?.endEncoding()
+        
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
     }
 }
